aaf156d7335449acef60a89b9721af184adab96a
YousseMk <youssef.abdelmaksoud@ucalgary.ca>
2020-11-25 09:42:46 -0700

    Create DBController.java

---
+++ src/Controllers/DBController.java
@@ -0,0 +1,44 @@
+package Controllers;
+
+//username is root
+//password is password
+
+import java.sql.*;
+
+
+public class DBController {
+
+    private Connection con = null;
+
+    public void connectToDB(){
+
+        try{
+            String url = "jdbc:mysql://localhost:3306";
+            con = DriverManager.getConnection(url, "root", "password");
+
+            System.out.println("connected to the db!");
+        }
+        catch(SQLException e){
+            throw new Error("problem connecting to database", e);
+        }
+    }
+
+    public void disconnectFromDB(){
+        try{
+            if(con != null){
+                con.close();
+            }
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+}
