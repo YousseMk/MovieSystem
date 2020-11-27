f4ef5a0907d25db80a13c6eb45d67dfb2eb94d48
YousseMk <youssef.abdelmaksoud@ucalgary.ca>
2020-11-27 09:46:43 -0700

    Create ShowTime.java

---
+++ src/Model/ShowTime.java
@@ -0,0 +1,13 @@
+package Model;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class ShowTime {
+    Date date;
+    Time start;
+    Time end;
+    Movie movie;
+    Theatre theatre;
+
+}
