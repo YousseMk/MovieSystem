6e5569e1d4d5aec6ceb992b20042b9ee204c502c
YousseMk <youssef.abdelmaksoud@ucalgary.ca>
2020-11-27 09:46:55 -0700

    Create UserController.java

---
+++ src/Controllers/UserController.java
@@ -0,0 +1,12 @@
+package Controllers;
+
+import Model.RegisteredUser;
+
+public class UserController {
+    public void addMember(RegisteredUser u){
+
+    }
+    public void removeMember(RegisteredUser u){
+
+    }
+}
