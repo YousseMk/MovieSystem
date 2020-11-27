8a75f13f882602ab2b3e9e9502b0b238d7051e72
YousseMk <youssef.abdelmaksoud@ucalgary.ca>
2020-11-27 09:46:46 -0700

    Create RegisteredUser.java

---
+++ src/Model/RegisteredUser.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class RegisteredUser extends User {
+    private Account account;
+
+    public RegisteredUser(){
+        //connect to db pull info, set to super
+        //pul account set to account
+        super();
+
+
+    }
+
+    public login(){
+
+    }
+}
