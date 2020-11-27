1802fcf8274b940a7c6e1523cd5122eaabfa8ac6
YousseMk <youssef.abdelmaksoud@ucalgary.ca>
2020-11-27 09:46:59 -0700

    Create BookingController.java

---
+++ src/Controllers/BookingController.java
@@ -0,0 +1,17 @@
+package Controllers;
+
+public class BookingController {
+
+    public void displayTheatreInfo(Booking b){
+
+    }
+    public void displayMovie(Booking b){
+
+    }
+    public void displayShowtime(Booking b){
+
+    }
+    public void displaySeatGrid(Booking b){
+
+    }
+}
