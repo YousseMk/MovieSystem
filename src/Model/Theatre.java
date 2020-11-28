bb241f7b0d6780ce00bbd8cc5fa654b2a7feab00
YousseMk <youssef.abdelmaksoud@ucalgary.ca>
2020-11-28 01:33:10 -0700

    Update Theatre.java

--- src/Model/Theatre.java
+++ src/Model/Theatre.java
@@ -2,13 +2,13 @@ package Model;
 
 public class Theatre {
 
-	  private String name;
+    private String name;
     private Seat seatGrid; //array?
     private int tNum;
     private int cap;
 
     public Theatre(int num, int c, String n){
-    	seatGrid = new Seat();
+    	//seatGrid = new Seat();
     	tNum = num;
     	cap = c;
     	name = n;
