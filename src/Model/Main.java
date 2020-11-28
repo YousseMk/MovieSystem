c181d3fdc0bc5c2aea38425d559e47316afd2e0e
YousseMk <youssef.abdelmaksoud@ucalgary.ca>
2020-11-28 01:33:22 -0700

    Update Main.java

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -1,9 +1,21 @@
 package Model;
 import java.util.ArrayList;
 import Controllers.UserController;
+import View.LoginView;
 
 public class Main {
     public static void main(String[] args){
-    	ArrayList <Theatre> theatres = new ArrayList <Theatre>();
+
+        //ArrayList <Theatre> theatres = new ArrayList <Theatre>();
+
+        Login l = new Login();
+        User newuser = null;
+        newuser = l.login();
+
+        System.out.println(newuser.getEmail());
+
+
+
+
     }
 }
