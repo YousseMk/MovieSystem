d8ef335efb79c316b7c9ef0f22da0af1e9a22de3
HashirA123 <59901350+HashirA123@users.noreply.github.com>
2020-11-28 13:25:32 -0700

    just added some stuff to GuestUser

--- src/Model/GuestUser.java
+++ src/Model/GuestUser.java
@@ -1,5 +1,7 @@
 package Model;
 
+import Controllers.UserController;
+
 public class GuestUser extends User{
     private String tempId;
 
@@ -8,9 +10,19 @@ public class GuestUser extends User{
         tempId = t;
     }
 
-    public void CreateAccount(){
+    // this function will need to be passed the information read from the View section
+    public void CreateAccount(String Username, String Password, String card){
+        UserController Ucontroller= new UserController();
         // this function will prompt the user to enter a
         // username and password using a GUI screen.
-
+        // first create new account, then create the registered user
+        // then call addMember from UserController.
+        // we will maybe have a Global variable for MemberID, that we can simply increase it for
+        // each new member, not having to worry about copies.
+        int NewID = (int)Math.random();         // change this later.
+        Account a = new Account(Username,Password,card);
+        RegisteredUser newReg = new RegisteredUser(this.getName(), this.getEmail(), this.getAddress(), a, NewID)
+        // add this user into the database
+        Ucontroller.addMember(newReg);
     }
 }
