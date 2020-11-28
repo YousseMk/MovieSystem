4122e09e2ccd0926ea08340a2d2bd90ba5994e75
YousseMk <youssef.abdelmaksoud@ucalgary.ca>
2020-11-27 17:07:27 -0700

    Update LoginController.java

--- src/Controllers/LoginController.java
+++ src/Controllers/LoginController.java
@@ -1,7 +1,50 @@
 package Controllers;
 
+import Model.Account;
+import Model.RegisteredUser;
+import Model.User;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class LoginController {
     public void showLoginScreen(){
+        //calls the login view function
+    }
 
+    public Model.RegisteredUser attemptLogin(String username, String password){
+        //the view will prompt user for username and password
+        //the program will check the database for a user with the provided username and password
+        //if the username or password is unavailable, error message will be displayed else we log them in
+        DBController db = new DBController();
+        ResultSet rs;
+        RegisteredUser returnedUser = null;
+
+
+        try {
+
+                db.connectToDB();
+                String stmt = "select * from RegisteredUser R where R.username=? and R.password = ?;";
+
+                PreparedStatement s = db.getCon().prepareStatement(stmt);
+                s.setString(1, username);
+                s.setString(2, password);
+                rs = s.executeQuery();
+
+                while(rs.next()) {
+                    Account userAccount = new Account(rs.getString(5), rs.getString(6), rs.getString(7));
+
+                    returnedUser = new RegisteredUser(rs.getString(1), rs.getString(2), rs.getString(3),
+                            userAccount, rs.getInt(4));
+
+                    db.disconnectFromDB();
+                    return returnedUser;
+                }
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        return returnedUser;
     }
 }
