be41fa2dd05c3eded70bb62e61a81ce795455063
YousseMk <youssef.abdelmaksoud@ucalgary.ca>
2020-11-29 04:41:41 -0700

    Update BookingController.java

--- src/Controllers/BookingController.java
+++ src/Controllers/BookingController.java
@@ -1,19 +1,30 @@
 package Controllers;
 
-import Model.Booking;
+
+import Model.MovieTicket;
 
 public class BookingController {
 
-    public void displayTheatreInfo(Booking b){
-
+    /*public void selectTheatre(MovieTicket ticket){
+        //give ticket to view so that theatre info can be selected
+        ticket.setTheatreName(name);
+        ticket.setNum(tnum);
     }
-    public void displayMovie(Booking b){
 
+    public void selectMovie(MovieTicket ticket){
+        //give ticket to view to select movie
+        ticket.setMovieName(moviename);
     }
-    public void displayShowtime(Booking b){
 
+    public void selectShowtime(MovieTicket ticket){
+        //give ticket to view to select showtime
+        ticket.setStartTime(start);
+        ticket.setEndTime(end);
     }
-    public void displaySeatGrid(Booking b){
 
-    }
+    public void selectSeat(MovieTicket ticket){
+        //give ticket to view to select seat
+        BookingController bc = new BookingController();
+        bc.selectSeat(ticket);
+    }*/
 }
